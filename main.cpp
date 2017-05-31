17f9053ba95690155b62458b18024fa433b9f828
Miguel Lorenzo <miguel.lorenzo.1990@gmail.com>
2017-05-31 10:57:16 -0500

    SMike beta

---
+++ main.cpp
@@ -0,0 +1,328 @@
+#include <iostream>
+#include "asio.hpp"
+#include <opencv2/opencv.hpp>
+#include "extraccion_codificacion.h"
+
+using namespace std;
+using namespace asio;
+using namespace cv;
+
+vector<char> cuadro_global(1024*1024);
+
+/**consiste de un socket cliente, un puerto serial, un nombre de servicio a proveer*/
+class fwd
+{
+public:
+  fwd(asio::io_service& io_service, std::string ip, std::string puerto_tcp) :
+    iosvc_(io_service),
+    socket_(io_service),
+    temporizador_(io_service)
+  {
+    asio::ip::tcp::resolver resolvedor(io_service);
+    asio::ip::tcp::resolver::query query(ip, puerto_tcp); //puedes meter dns aqui
+    asio::ip::tcp::resolver::iterator iter = resolvedor.resolve(query);
+    endpoint_ = iter->endpoint();
+    tx_buf_socket_.clear();
+  }
+  void iniciar()
+  {
+    iniciar_temporizador();
+    conectar_socket();
+  }
+
+  void conectar_socket();
+  void leer_socket();
+  void escribir_socket(std::string str);
+
+
+private:
+  void iniciar_temporizador();
+
+  asio::io_service& iosvc_;
+
+  std::array<char,8192> rx_buf_socket_;
+  std::string tx_buf_socket_;
+  asio::ip::tcp::socket socket_;
+  asio::ip::tcp::endpoint endpoint_;
+
+  asio::steady_timer temporizador_;
+  bool cerrar_{false};
+};
+
+/**                 socket                       */
+/**Función miembro que verifica cada 25ms si hay algo que debamos enviar al servidor*/
+void fwd::iniciar_temporizador()
+{
+  asio::error_code ec;
+  temporizador_.expires_from_now(std::chrono::milliseconds(2000), ec);
+  temporizador_.async_wait([this](std::error_code ec)
+  {
+    if(!ec)
+    {
+      Mat m(ScreenCap_Ipl());
+      escribir_socket(codificar(m));
+    }
+
+    else
+      std::cout << "Error temporizador: " << ec.value() <<  ": " <<  ec.message() << std::endl;
+
+    if(!cerrar_)
+      iniciar_temporizador();
+  });
+}
+
+
+void fwd::conectar_socket()
+{
+  socket_.async_connect(endpoint_, [this](error_code ec)
+  {
+    if(!ec)
+    {
+      cout << "conectado a " << this->socket_.remote_endpoint().address().to_string()
+          << ":" << this->socket_.remote_endpoint().port() << '\n';
+      string login="mike;ftw;";
+      cout << "LOGIN: " << login;
+      escribir_socket(login);
+      Sleep(200);
+      string peticion="ofrecer desk";
+      escribir_socket(peticion);
+
+      leer_socket();
+    }
+
+    else
+    {
+      cout << "Error conectando: " << ec.value() <<  ": " <<  ec.message() << '\n';
+      if(ec.value() == 10056) /*Error 10056: Se solicitó conexión en socket ya conectado*/
+      {
+        std::error_code ec_cerrar;
+        socket_.close(ec_cerrar);
+        if(!ec_cerrar)
+          conectar_socket();
+        else
+          std::cout << "Error cerrando y conectando: " << ec_cerrar.value() <<  ": " <<  ec_cerrar.message() << std::endl;
+      }
+        //...
+    }
+  });
+} //conectar
+
+void fwd::leer_socket()
+{
+  socket_.async_read_some(asio::buffer(rx_buf_socket_),
+    [this](std::error_code ec, std::size_t bytes_leidos)
+  {
+    if(!ec)
+    {
+      cout.write(rx_buf_socket_.data(), bytes_leidos);
+
+      memset(rx_buf_socket_.data(), '\0', rx_buf_socket_.size() );
+      leer_socket();
+    }
+    else
+    {
+      cout << "Error leyendo socket->" << ec.value() <<  ": " << ec.message() << std::endl;
+      cerrar_=true;
+    }
+  });
+}
+
+void fwd::escribir_socket(std::string str)
+{
+  tx_buf_socket_ = str;
+  //cout << str;
+  asio::async_write(socket_, asio::buffer(tx_buf_socket_.data(), tx_buf_socket_.size()),
+    [this](std::error_code ec, std::size_t len)
+  {
+    if (!ec)
+    {
+      cout << "Se escribieron " << len << "bytes" << '\n';
+      /*procesar escritura exitosa*/
+    }
+  });
+}
+
+
+/*************************************************************************/
+/*************************************************************************/
+/*************************************************************************/
+
+class receptor
+{
+public:
+  receptor(asio::io_service& io_service, std::string ip, std::string puerto_tcp) :
+    iosvc_(io_service),
+    socket_(io_service),
+    temporizador_(io_service)
+  {
+    asio::ip::tcp::resolver resolvedor(io_service);
+    asio::ip::tcp::resolver::query query(ip, puerto_tcp); //puedes meter dns aqui
+    asio::ip::tcp::resolver::iterator iter = resolvedor.resolve(query);
+    endpoint_ = iter->endpoint();
+    cuadro_.reserve(1024*1024);
+  }
+  void iniciar()
+  {
+    conectar_socket();
+  }
+
+  void conectar_socket();
+  void leer_socket();
+  void escribir_socket(std::string str);
+
+
+private:
+  void iniciar_temporizador();
+
+  asio::io_service& iosvc_;
+
+  std::array<char,8192> rx_buf_socket_;
+  std::string tx_buf_socket_;
+  asio::ip::tcp::socket socket_;
+  asio::ip::tcp::endpoint endpoint_;
+
+  asio::steady_timer temporizador_;
+
+  std::vector<char> cuadro_;
+};
+
+
+void receptor::conectar_socket()
+{
+  socket_.async_connect(endpoint_, [this](error_code ec)
+  {
+    if(!ec)
+    {
+      cout << "conectado a " << this->socket_.remote_endpoint().address().to_string()
+          << ":" << this->socket_.remote_endpoint().port() << '\n';
+      escribir_socket("nube;nube;");
+      Sleep(200);
+      escribir_socket("suscribir desk");
+
+      leer_socket();
+    }
+
+    else
+    {
+      cout << "Error conectando: " << ec.value() <<  ": " <<  ec.message() << '\n';
+      if(ec.value() == 10056) /*Error 10056: Se solicitó conexión en socket ya conectado*/
+      {
+        std::error_code ec_cerrar;
+        socket_.close(ec_cerrar);
+        if(!ec_cerrar)
+          conectar_socket();
+        else
+          std::cout << "Error cerrando y conectando: " << ec_cerrar.value() <<  ": " <<  ec_cerrar.message() << std::endl;
+      }
+        //...
+    }
+  });
+} //conectar
+
+void receptor::leer_socket()
+{
+  socket_.async_read_some(asio::buffer(rx_buf_socket_),
+    [this](std::error_code ec, std::size_t bytes_leidos)
+  {
+    if(!ec)
+    {
+      cout << "cuadro_.size()=" << cuadro_.size() << "+ bytes_leidos=" << bytes_leidos ;
+      cuadro_.insert(cuadro_.end(), rx_buf_socket_.begin(), rx_buf_socket_.begin() + bytes_leidos);
+      cout << " ==> cuadro.size()=" << cuadro_.size() << '\n';
+      //cout.write(rx_buf_socket_.data(), bytes_leidos);
+      cout << "ultimos 2 bytes: " << (int)rx_buf_socket_[bytes_leidos-2] <<" "<<(int)rx_buf_socket_[bytes_leidos-1] <<"\n";
+      if((int)rx_buf_socket_[bytes_leidos-2]==-1 && (int)rx_buf_socket_[bytes_leidos-1] == -39 )
+      {
+        cout << "fin de frame. cuadro_=" <<cuadro_.size() << '\n' ;//<< "\tframe w,h="<< frame.cols << "," <<frame.rows <<"\n";
+        cuadro_global=cuadro_;
+        cuadro_.clear();
+      }
+
+
+      memset(rx_buf_socket_.data(), '\0', rx_buf_socket_.size() );
+      leer_socket();
+    }
+    else
+    {
+      cout << "Error leyendo socket->" << ec.value() <<  ": " << ec.message() << std::endl;
+    }
+  });
+}
+
+void receptor::escribir_socket(std::string str)
+{
+  tx_buf_socket_ = str;
+  asio::async_write(socket_, asio::buffer(tx_buf_socket_.data(), tx_buf_socket_.size()),
+    [this](std::error_code ec, std::size_t len)
+  {
+    if (!ec)
+    {
+      //cout << "Se escribio " << tx_buf_ << " con " << len << " bytes" << endl;
+      /*procesar escritura exitosa*/
+    }
+  });
+}
+
+void renderizar()
+{
+  cv::redirectError(solucionador_de_pedos);
+  namedWindow("cuadro"); //una cicatriz en la cara de París
+  while(waitKey(300) != 'q')
+  {
+    if(cuadro_global.size() > 0)
+    {
+      try
+      {
+        Mat frame = cv::imdecode(cuadro_global, CV_LOAD_IMAGE_GRAYSCALE);
+        imshow("cuadro",frame);
+      }
+      catch(std::exception const& e)
+      {
+        cout << ".";// << e.what();
+      }
+    }
+
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  if(argc != 2)
+  {
+    cout << argc <<" Por favor ingresa como argumento del programa \"cliente\" o \"servidor\" dependiendo de que quieras\n";
+    exit(0);
+  }
+
+  try
+  {
+    io_service servicio;
+
+    if(strcmp( argv[1], "servidor") == 0)
+    {
+      fwd escaner_escritorio(servicio, "201.139.98.214", "3214");
+      escaner_escritorio.iniciar();
+      servicio.run();
+    }
+
+    else if(strcmp( argv[1], "cliente") == 0)
+    {
+      thread t(renderizar);
+      receptor receptor_escritorio(servicio, "201.139.98.214", "3214");
+      receptor_escritorio.iniciar();
+      servicio.run();
+      t.join();
+    }
+
+    else
+    {
+      cout << "\nPor favor ingresa una opcion correcta\n";
+    }
+
+  }
+  catch(std::exception const& e)
+  {
+    cout << e.what() << '\n';
+  }
+
+  return 0;
+}
---
+++ src/extraccion_codificacion.cpp
@@ -0,0 +1,112 @@
+#include "extraccion_codificacion.h"
+
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <windows.h>
+#include <stdexcept>
+#include <iostream>
+#include <thread>
+
+using namespace cv;
+using namespace std;
+
+IplImage* ScreenCap_Ipl()
+{
+  IplImage *img;
+  int nWidth;
+  int nHeight;
+  HWND hWnd;
+
+  hWnd = GetDesktopWindow();
+  if(hWnd == NULL)
+  {
+    throw std::runtime_error("error capturando escritorio");
+  }
+  /*
+  if((hWnd =FindWindow( NULL, "Play Live Chess - Chess.com - Google Chrome")) == NULL)
+  {
+    printf("No encontre la ventana :(\n");
+    return 0;
+  }*/
+
+  GetLastError();
+  HDC hDC = GetDC(hWnd);
+  HDC hMemDC = CreateCompatibleDC(hDC);
+  RECT rect;
+
+  if(GetWindowRect(hWnd, &rect) )
+  {
+    nWidth = rect.right - rect.left;
+    nHeight = rect.bottom - rect.top;
+  }
+
+  HBITMAP hBitmap = CreateCompatibleBitmap( hDC, nWidth, nHeight);
+  BITMAPINFO bmi;
+  ZeroMemory(&bmi, sizeof(bmi));
+  bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+  bmi.bmiHeader.biWidth = nWidth;
+  bmi.bmiHeader.biHeight = -(nHeight);
+  bmi.bmiHeader.biBitCount = 32;
+  bmi.bmiHeader.biPlanes = 1;
+  bmi.bmiHeader.biCompression = BI_RGB;
+  bmi.bmiHeader.biSizeImage = 32 * nWidth * nHeight / 8;
+  BYTE *pbBits = new BYTE[bmi.bmiHeader.biSizeImage];
+
+  if( hBitmap)
+  {
+    HBITMAP hOld = (HBITMAP)SelectObject( hMemDC, hBitmap);
+    std::cout << "ancho" << nWidth << " -- altura" <<nHeight << '\n';
+    BitBlt( hMemDC, 0, 0, nWidth, nHeight, hDC, 0, 0, SRCCOPY);
+    SelectObject( hMemDC, hOld);
+    BITMAP bbmp;
+    GetObject( hBitmap, sizeof(BITMAP), &bbmp);
+    int nChannels = bbmp.bmBitsPixel == 1 ? 1 : bbmp.bmBitsPixel/8 ;
+    int depth = bbmp.bmBitsPixel == 1 ? IPL_DEPTH_1U : IPL_DEPTH_8U;
+    GetDIBits(hDC, hBitmap, 0, nHeight, pbBits, &bmi,DIB_RGB_COLORS);
+    DeleteObject(hBitmap);
+    img = cvCreateImageHeader( cvSize(bbmp.bmWidth, bbmp.bmHeight), depth, nChannels );
+    img ->imageData = (char*)malloc(bbmp.bmHeight * bbmp.bmWidth * nChannels * sizeof(char));
+    memcpy( img->imageData,(char*)(pbBits), bbmp.bmHeight * bbmp.bmWidth *
+    nChannels);
+  }
+
+  delete[] pbBits;
+
+  ::DeleteDC(hDC);
+  ::DeleteDC(hMemDC);
+  ::DeleteObject(hWnd);
+
+  return img;
+}
+
+std::string codificar(cv::Mat& src)
+{
+  //cout << src.cols*src.rows*3 << '\t';
+  vector<unsigned char> buffer;
+  cvtColor(src, src, CV_BGR2GRAY);
+  const float factor_rsz=0.75;
+  Size nvo_tam(src.cols*factor_rsz, src.rows*factor_rsz);
+  resize(src,src,nvo_tam);
+  vector<int> compression_params;
+    compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+    compression_params.push_back(50);
+
+  cv::imencode(".jpg", src, buffer,compression_params);
+  cout << buffer.size() << '\t'; //217 es D9, lo cual es correcto. El end of frame es FFD9
+
+  //Mat frame = cv::imdecode(buffer, CV_LOAD_IMAGE_COLOR);
+  //cout << "\tframe w,h="<< frame.cols << "," <<frame.rows <<"\n";
+
+  //imshow("cuadro", frame);
+  //waitKey(15); //con 5 no le da tiempo de llenar el cuadro
+  //thread t(mostrar, std::ref(frame));
+  //t.detach();
+
+  return string(buffer.begin(), buffer.end());
+  //return string(src.begin<unsigned char>(), src.end<unsigned char>());
+}
+
+int solucionador_de_pedos(int i, const char* c1, const char* c2, const char* c3, int j, void* pf)
+{
+  return 0;
+}
